72f837576b2768ca70fad03cb823ab5f11ed6603
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-07-11 02:31:33 -0700

    Working on the background

--- src/components/QuizWrapper.jsx
+++ src/components/QuizWrapper.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import "./QuizWrapper.css";
 
 const QuizWrapper = () => {
   return (
     <div id="quiz">
-      <h1>Quiz app</h1>
-      <p>Built using react js</p>
+      <div
+        id="background-wraper"
+        className="w-full h-screen flex items-center justify-center"
+      >
+        <div id="quiz-wrapper"></div>
+      </div>
     </div>
   );
 };
